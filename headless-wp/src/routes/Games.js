4f22085f4c3a93f1bae52e0db81e24dafb848df5
BHess2653 <GM_Dahaka@BHMac.local>
2018-05-14 16:41:33 -0400

    added eslint and made changes for eslint

--- headless-wp/src/routes/Games.js
+++ headless-wp/src/routes/Games.js
@@ -5,35 +5,35 @@ class Game extends Component {
   constructor() {
     super();
     this.state = {
-      games: []
-    }
+      games: [],
+    };
   }
-componentDidMount() {
-    let dataURL = "http://localhost:8888//wp-json/wp/v2/games?_embed";
+  componentDidMount() {
+    const dataURL = 'http://localhost:8888//wp-json/wp/v2/games?_embed';
     fetch(dataURL)
       .then(res => res.json())
-      .then(res => {
+      .then((res) => {
         this.setState({
-          games: res
-        })
-      })
+          games: res,
+        });
+      });
   }
-render() {
-    let games = this.state.games.map((game, index) => {
+  render() {
+    const games = this.state.games.map((game, index) => {
       return <div className="Game-div" key={index}>
-        <img className="Game-img" src={game._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} />
+        <img className="Game-img" alt="" src={game._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} />
         <p><strong>Title:</strong> {game.title.rendered}</p>
         <p><strong>Release Year:</strong> {game.acf.release_year}</p>
         <p><strong>imdb Rating:</strong> {game.acf.rating}</p>
-        <div><strong>Synopsis:</strong><div dangerouslySetInnerHTML={ {__html: game.acf.description} } /></div>
-      </div>
+        <div><strong>Synopsis:</strong><div dangerouslySetInnerHTML={{ __html: game.acf.description }} /></div>
+      </div>;
     });
-return (
+    return (
       <div className="Game">
         <h2 className="Game-title">Games</h2>
         {games}
       </div>
-    )
+    );
   }
 }
 export default Game;
--- headless-wp/src/routes/Movies.js
+++ headless-wp/src/routes/Movies.js
@@ -5,35 +5,35 @@ class Movie extends Component {
   constructor() {
     super();
     this.state = {
-      movies: []
-    }
+      movies: [],
+    };
   }
-componentDidMount() {
-    let dataURL = "http://localhost:8888//wp-json/wp/v2/movies?_embed";
+  componentDidMount() {
+    const dataURL = 'http://localhost:8888//wp-json/wp/v2/movies?_embed';
     fetch(dataURL)
       .then(res => res.json())
-      .then(res => {
+      .then((res) => {
         this.setState({
-          movies: res
-        })
-      })
+          movies: res,
+        });
+      });
   }
-render() {
-    let movies = this.state.movies.map((movie, index) => {
+  render() {
+    const movies = this.state.movies.map((movie, index) => {
       return <div className="Movie-div" key={index}>
-        <img className="Movie-img" src={movie._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} />
+        <img className="Movie-img" alt="" src={movie._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} />
         <p><strong>Title:</strong> {movie.title.rendered}</p>
         <p><strong>Release Year:</strong> {movie.acf.release_year}</p>
         <p><strong>Rating:</strong> {movie.acf.rating}</p>
-        <div><strong>Description:</strong><div dangerouslySetInnerHTML={ {__html: movie.acf.description} } /></div>
-      </div>
+        <div><strong>Description:</strong><div dangerouslySetInnerHTML={{ __html: movie.acf.description }} /></div>
+      </div>;
     });
-return (
+    return (
       <div className="Movie">
         <h2 className="Movie-title">Movies</h2>
         {movies}
       </div>
-    )
+    );
   }
 }
 export default Movie;
